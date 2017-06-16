dc972d064ade4fe519d49c35af38ddcdc486f2d1
Sebastian McKenzie <sebmck@fb.com>
2017-06-16 14:30:00 +0100

    switch to express

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const request = require('request-promise-native');
+
+const app = express();
+app.use(express.static('static'));
+
+const requestCF = url => request({
+  url,
+  headers: {
+    'X-Auth-Email': process.env.CF_EMAIL,
+    'X-Auth-Key': process.env.CF_KEY,
+    'Content-Type': 'application/json',
+  },
+});
+
+const cachePromise = (producer, interval) => {
+  let lastUpdatedAt;
+  let lastUpdatedPromise;
+
+  return function() {
+    const now = Date.now();
+    const hasExpired = !lastUpdatedAt || lastUpdatedAt + interval < now;
+
+    if (hasExpired) {
+      lastUpdatedAt = now;
+      return lastUpdatedPromise = producer();
+    } else {
+      return lastUpdatedPromise;
+    }
+  };
+};
+
+const getStats = cachePromise(async () => {
+  console.log('Fetching CF stats...');
+
+  const [cfData, npmData] = await Promise.all([
+    requestCF(`https://api.cloudflare.com/client/v4/zones/${process.env.CF_ZONE}/analytics/dashboard?since=-43200&continuous=true`),
+    request('https://api.npmjs.org/downloads/point/last-month')
+  ]).then(result => result.map(item => JSON.parse(item)));
+
+  const yarnDownloads = cfData.result.totals.requests.content_type['octet-stream'];
+  const npmDownloads = npmData.downloads;
+  return {
+    cfStats: cfData.result,
+    yarnDownloads: yarnDownloads,
+    percentage: Math.round(yarnDownloads / npmDownloads * 10000) / 100,
+  };
+}, 60 * 1000);
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  getStats().then(data => {
+    res.render('index', data);
+  });
+});
+
+const port = 3000 || process.env.PORT;
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`)
+});
